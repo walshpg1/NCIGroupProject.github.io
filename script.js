0ad2ed5e42ce83a750c3c67a6154f791b343bdd7
Paddy Walsh <walshpg1@gmail.com>
2024-11-24 12:41:51 +0000

    added the first files  index style and JS

---
+++ script.js
@@ -0,0 +1,28 @@
+window.onload = function () {
+    var menuToggle = document.querySelector(".menu-toggle");
+    var menu = document.getElementById("menu");
+
+    menuToggle.onclick = function () {
+        if (menu.className === "active") {
+            menu.className = "";
+        } else {
+            menu.className = "active";
+        }
+    };
+};
+
+
+window.onload = function () {
+    var themeToggle = document.querySelector(".theme-toggle");
+    var body = document.body;
+
+    themeToggle.onclick = function () {
+        if (body.className === "dark-mode") {
+            body.className = "light-mode";
+            themeToggle.textContent = "🌛";
+        } else {
+            body.className = "dark-mode";
+            themeToggle.textContent = "🌞";
+        }
+    };
+};
